c2aff2df321bc75430f761f82adc520cdd88be63
a314159265 <abhash.subedi@renegadeinsurance.com>
2026-07-13 14:48:03 +0545

    working code

--- python_task_1/task_1.py
+++ python_task_1/task_1.py
@@ -4,7 +4,42 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent
 FILE_PATH = ROOT_DIR / "data" / "expenses_sample.csv"
 
-def save(date:str, category:str, amount:float, description:str):
+expenses = []
+
+def save():
     with open(FILE_PATH, "w") as f:
-        f.write(f'{date}, {category}, {amount}, {description}\n')
-        
+        f.writelines(expenses)
+
+def load():
+    with open(FILE_PATH, 'r') as file:
+        next(file)
+        for line in file:
+            expenses.append(line)
+
+def show_full():
+    
+    for expense in expenses:
+        print(expense)
+    
+
+def show_summary():
+    summary = {}
+    for expense in expenses:
+        date, category, amount,  description = expense.strip().split(',')
+        if category not in summary:
+            summary[category] = 0
+        summary[category] += float(amount)
+    print(summary)
+
+def add(date:str, category:str, amount:float, description:str):
+    expenses.append(f'{date},{category},{amount},{description}\n')
+    
+
+def main():
+    load()
+    show_summary()
+    add("2023-10-01", "Food", 50.0, "Grocery shopping")
+    save()
+    show_summary()
+
+main()
