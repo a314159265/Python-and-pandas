ae4067e8348d387df6819a25b088590a2126bcbd
a314159265 <abhash.subedi@renegadeinsurance.com>
2026-07-13 16:51:21 +0545

    working code

--- python_task_1/task_1.py
+++ python_task_1/task_1.py
@@ -24,12 +24,24 @@ def show_full():
 
 def show_summary():
     summary = {}
+    min = 100000
+    max = -100000
+    no_expenses = len(expenses)
     for expense in expenses:
         date, category, amount,  description = expense.strip().split(',')
         if category not in summary:
             summary[category] = 0
-        summary[category] += float(amount)
+        amount = float(amount)
+        summary[category] += amount
+        if amount < min:
+            min = amount
+        if amount > max:
+            max = amount
+        avg = sum(summary.values()) / no_expenses if no_expenses > 0 else 0
     print(summary)
+    print(f"Minimum amount: {min}")
+    print(f"Maximum amount: {max}")
+    print(f"Average amount: {avg}")
 
 def add(date:str, category:str, amount:float, description:str):
     expenses.append(f'{date},{category},{amount},{description}\n')
@@ -37,8 +49,8 @@ def add(date:str, category:str, amount:float, description:str):
 
 def main():
     response = input('Enter what you would like to do \n type 1 for adding bill' \
-    '\n type 2 for showing all bills \n type 3 for showing summary \n type 4 for save \n type 5 for quit \n')
-    while response != '5':
+    '\n type 2 for showing all bills \n type 3 for showing summary \n type 4 for save \n type 5 for load \n type 6 to quit \n')
+    while response != '6':
         if response == '1':
             date = input('Enter the date of the bill (YYYY-MM-DD): ')
             category = input('Enter the category of the bill: ')
@@ -51,9 +63,11 @@ def main():
             show_summary()
         elif response == '4':
             save()
+        elif response == '5':
+            load()
         else:
             print('Invalid option. Please try again.')
         
-        response = input('Enter what you would like to do \n type 1 for adding bill')
+        main()
 
 main()
